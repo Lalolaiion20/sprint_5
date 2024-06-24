6f58795466175cf7f25881316a01a0868caa8874
Lalolaiion20 <151203750+Lalolaiion20@users.noreply.github.com>
2024-06-24 14:49:40 -0600

    Update app.py

--- app.py
+++ app.py
@@ -1,8 +1,8 @@
-import streamlit as st 
+import streamlit as st
 import pandas as pd
 import plotly_express as px
 
-st.heater('informacion para la venta de coches')
+st.header('informacion para la venta de coches')
 
 data_base = pd.read_csv('vehicles_us.csv') ## Lee el archivo CSV
 
