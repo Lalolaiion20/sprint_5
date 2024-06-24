7405b84f36e7aad528a0298a07ffed22f3871ceb
Lalolaiion20 <eduardo.leong@hotmail.com>
2024-06-24 09:19:41 -0600

    add notes file

---
+++ app.py
@@ -0,0 +1,28 @@
+import streamlit as st 
+import pandas as pd
+import plotly_express as px
+
+data_base = pd.read_csv('vehicles_us.csv') ## Lee el archivo CSV
+
+hist_button = st.hist_button('construir histograma')
+
+build_histogram = st.checkbox('Construir un histograma')
+
+
+if hist_button:
+    
+    st.write('Creación de un histograma para el conjunto de datos de anuncios de venta de coches')
+
+    fig = px.histogram(data_base,x='odomenter')
+
+    st.plotly_chart(fig,use_container_width=True)
+    
+if build_histogram: # si la casilla de verificación está seleccionada
+    
+    st.write('Construir un histograma para la columna odómetro')
+    
+    fig = px.histogram(data_base,x='odomenter')
+
+    st.plotly_chart(fig,use_container_width=True)
+
+
