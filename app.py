8cd55f0471417018c4e2485fa89433207f7fe089
Lalolaiion20 <151203750+Lalolaiion20@users.noreply.github.com>
2024-06-24 14:55:26 -0600

    Update app.py

--- app.py
+++ app.py
@@ -16,7 +16,7 @@ disp_cost = st.button('Construir gráfico de dispersión')
 
 if disp_cost:
     st.write('Creación de un gráfico de dispersión para el conjunto de datos de anuncios de venta de coches')
-    fig = px.scatter(car_data, x="odometer", y="price")
+    fig = px.scatter(data_base, x="odometer", y="price")
     st.plotly_chart(fig, use_container_width=True)
 
 if hist_button:
